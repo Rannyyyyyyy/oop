842d2f4ac712762d289e36db5542fde114766041
Rannyyyyyyy <23-18644@g.batstate-u.edu.ph>
2024-11-08 15:16:09 +0800

    Update InheritanceSample.java

--- InheritanceSample.java
+++ InheritanceSample.java
@@ -4,13 +4,16 @@ public class InheritanceSample
 	public static void main(String[] args) {
 		Student student1 = new Student("Juan Luna", "Gumamela, Balayan, Batangas", "BSIT", 2);
 		System.out.println("Student Name: " + student1.getName());
-	    System.out.println("Student Address: " + student1.getAddress());
-	    System.out.println("Student Program: " + student1.getProgram());
-		System.out.println("Student Year: " + student1.getYear());
+	        System.out.println("Student Address: " + student1.getAddress());
+	        System.out.println("Student Program: " + student1.getProgram());
+                System.out.println("Student Year: " + student1.getYear());
+	        System.out.println());
+
+		
 		
 		Staff staff1 = new Staff("Marc Loyd Bautista", "Gumamela, Balayan, Batangas", "Batangas State University", 500.00f );
 		System.out.println("Staff Name: " + staff1.getName());
-	    System.out.println("Address: " + staff1.getAddress());
+	        System.out.println("Address: " + staff1.getAddress());
 		System.out.println("School: " + staff1.getSchool());
 		System.out.println("Salary: " + staff1.getSalary());
 
